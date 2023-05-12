d504ba3721d3dd3ae7fe3c208eb99f174acd7a53
asnecemnnit <asnecemnnit@gmail.com>
2023-05-12 11:07:07 +0200

    Updated dataset to load images from hdf5 file (annotations from pkl)

--- pytorchyolo/utils/data2hdf5.py
+++ pytorchyolo/utils/data2hdf5.py
@@ -7,69 +7,70 @@ import cv2
 import h5py
 
 
-#This code ist based on https://blade6570.github.io/soumyatripathy/hdf5_blog.html
-#and https://stackoverflow.com/questions/66631284/convert-a-folder-comprising-jpeg-images-to-hdf5/66641176#66641176
+# This code ist based on https://blade6570.github.io/soumyatripathy/hdf5_blog.html
+# and https://stackoverflow.com/questions/66631284/convert-a-folder-comprising-jpeg-images-to-hdf5/66641176#66641176
+
 
 def search_files(parent_folder, suffix):
-    paths = list(filter(lambda x: x.suffix==suffix , Path(parent_folder).rglob("**/*")))
+    paths = list(
+        filter(lambda x: x.suffix == suffix, Path(parent_folder).rglob("**/*"))
+    )
     num_files = len(paths)
     assert num_files > 0, "No samples found in %s" % parent_folder
     return paths, num_files
 
 
 def read_hdf5(hdf5_file, img_path):
-    hf = h5py.File(hdf5_file, 'r') # open a hdf5 file
-    img_bytes = np.array(hf[str(img_path)]) # read image in bytes
-    img = mmcv.imfrombytes(img_bytes, flag='color', channel_order='rgb') # convert to array
+    hf = h5py.File(hdf5_file, "r")  # open a hdf5 file
+    img_bytes = np.array(hf[str(img_path)])  # read image in bytes
+    img = mmcv.imfrombytes(
+        img_bytes, flag="color", channel_order="rgb"
+    )  # convert to array
     return img
 
-if __name__ == '__main__':
-     
+
+if __name__ == "__main__":
     # Create results directory
-    #hdf5_path = Path("./data/nuscenes/")
-    #hdf5_path.mkdir(parents=True, exist_ok=True)
-    #hdf5_path = hdf5_path.as_posix()
-    
-    
+    # hdf5_path = Path("./data/nuscenes/")
+    # hdf5_path.mkdir(parents=True, exist_ok=True)
+    # hdf5_path = hdf5_path.as_posix()
+
     # Look into the pkl files to see how the dataset is structured:
-    opened_pickle = open("data/nuscenes/nuscenes_infos_temporal_train.pkl", "rb")
-    loaded_train_pickle = pickle.load(opened_pickle)
-    opened_pickle = open("data/nuscenes/nuscenes_infos_temporal_val.pkl", "rb")
-    loaded_val_pickle = pickle.load(opened_pickle)   
-    
-    
+    # opened_pickle = open(
+    #     "data/nuscenes/nuscenes_infos_temporal_train.pkl", "rb"
+    # )
+    # loaded_train_pickle = pickle.load(opened_pickle)
+    # opened_pickle = open("data/nuscenes/nuscenes_infos_temporal_val.pkl", "rb")
+    # loaded_val_pickle = pickle.load(opened_pickle)
+
     # get number of samples and a list of filepaths in data folder
     data_folder = "data"
-    sample_paths, num_samples = search_files(data_folder, '.jpg')
-    
+    sample_paths, num_samples = search_files(data_folder, ".jpg")
 
-    save_path = './data/nuscenes/data.hdf5'
-    
-    index =1
+    save_path = "data/data.hdf5"
+
+    index = 1
     progress_frequency = 50
-    with h5py.File(save_path, 'w') as hf:
-        
-        for img_path in sample_paths[:30]: # save only the first 30 images for testing
-            
-            binary_img = open(str(img_path), 'rb').read()
+    with h5py.File(save_path, "w") as hf:
+        for img_path in sample_paths[
+            :30
+        ]:  # save only the first 30 images for testing
+            binary_img = open(str(img_path), "rb").read()
             binary_img_np = np.asarray(binary_img)
-            
+
             dset = hf.create_dataset(str(img_path), data=binary_img_np)
             if index % progress_frequency == 0:
                 print("[%d/%d]" % (index, num_samples))
             index = index + 1
 
-    print('hdf5 file size: %d bytes'%os.path.getsize(save_path))
-    
+    print("hdf5 file size: %d bytes" % os.path.getsize(save_path))
 
-    hdf5_file ='./data/nuscenes/data.hdf5'
-    
+    # hdf5_file ='./data/nuscenes/data.hdf5'
+    hdf5_file = save_path
+
     # read data from hf5 file and check size
     for img_path in sample_paths[:30]:
         img = read_hdf5(hdf5_file, img_path)
-        print ('img.shape', img.shape)    
-    
-    cv2.imwrite(('./test.jpg'),img)
-    
- 
-                 
+        print("img.shape", img.shape)
+
+    cv2.imwrite("data/test1.jpg", img)
--- pytorchyolo/utils/datasets.py
+++ pytorchyolo/utils/datasets.py
@@ -8,12 +8,15 @@ import warnings
 import numpy as np
 from PIL import Image
 from PIL import ImageFile
-import lmdb, pickle
+import lmdb, pickle, mmcv, h5py
 import cv2
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
-DB_PATH = "/private_shared/Projects/PyTorch-YOLOv3/data/custom/images_lmdb_test"
-PKL_PATH = "/private_shared/Projects/PyTorch-YOLOv3/data/custom/labels_test.pkl"
+# DB_PATH = "/private_shared/Projects/PyTorch-YOLOv3/data/custom/images_hdf5_test/images_test.hdf5"
+# PKL_PATH = "/private_shared/Projects/PyTorch-YOLOv3/data/custom/labels_test.pkl"
+PKL_PATH = "/private_shared/Projects/PyTorch-YOLOv3/data/custom/labels.pkl"
+# DB_PATH = "/public_shared/online/images_hdf5/images.hdf5"
+# PKL_PATH = "/public_shared/online/labels_pkl/labels.pkl"
 
 
 def pad_to_square(img, pad_value):
@@ -111,6 +114,8 @@ class ListDataset(Dataset):
             print(f"Could not read label '{label_path}'.")
             return
 
+        print(img, boxes)
+        # print(self.image_labels[index])
         # -----------
         #  Transform
         # -----------
@@ -151,7 +156,124 @@ class ListDataset(Dataset):
         return len(self.img_files)
 
 
-class my_dataset_LMDB(Dataset):
+class ListDataset_PKL(Dataset):
+    def __init__(
+        self, list_path, img_size=416, multiscale=True, transform=None
+    ):
+        with open(list_path, "r") as file:
+            self.img_files = file.readlines()
+
+        self.label_files = []
+        for path in self.img_files:
+            image_dir = os.path.dirname(path)
+            label_dir = "labels".join(image_dir.rsplit("images", 1))
+            assert label_dir != image_dir, (
+                "Image path must contain a folder named 'images'!"
+                f" \n'{image_dir}'"
+            )
+            label_file = os.path.join(label_dir, os.path.basename(path))
+            label_file = os.path.splitext(label_file)[0] + ".txt"
+            self.label_files.append(label_file)
+
+        self.img_size = img_size
+        self.max_objects = 100
+        self.multiscale = multiscale
+        self.min_size = self.img_size - 3 * 32
+        self.max_size = self.img_size + 3 * 32
+        self.batch_count = 0
+        self.transform = transform
+        self.pkl_path = PKL_PATH
+        self._init_pkl()
+
+    def _init_pkl(self):
+        with open(self.pkl_path, "rb") as handle:
+            self.key_labels = pickle.load(handle)
+
+    def read_pkl(self, key):
+        # print(key)
+        data_str = self.key_labels[key].decode("utf-8").split("\n")
+        label = np.array(
+            [list(map(float, s.split())) for s in data_str if s]
+        ).reshape(-1, 5)
+        # print(label)
+        return label
+
+    def __getitem__(self, index):
+        # ---------
+        #  Image
+        # ---------
+        try:
+            img_path = self.img_files[index % len(self.img_files)].rstrip()
+
+            img = np.array(Image.open(img_path).convert("RGB"), dtype=np.uint8)
+        except Exception:
+            print(f"Could not read image '{img_path}'.")
+            return
+
+        # ---------
+        #  Label
+        # ---------
+        img_label = img_path.split("/")[-1].split(".")[0]
+        # label_path = self.label_files[index % len(self.img_files)].rstrip()
+        try:
+            # Ignore warning if file is empty
+            # with warnings.catch_warnings():
+            #     warnings.simplefilter("ignore")
+            #     boxes_1 = np.loadtxt(label_path).reshape(-1, 5)
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                boxes = self.read_pkl(str(img_label))
+            # print(boxes_1, boxes_2)
+            # assert(boxes_1 == boxes_2)
+
+        except Exception:
+            print(f"Could not read label '{img_label}'.")
+            return
+
+        # print(img, boxes)
+        # print(self.image_labels[index])
+        # -----------
+        #  Transform
+        # -----------
+        if self.transform:
+            try:
+                # Ignore warning if file is empty
+                img, bb_targets = self.transform((img, boxes))
+            except Exception:
+                print("Could not apply transform.")
+                return
+
+        return img_path, img, bb_targets
+
+    def collate_fn(self, batch):
+        self.batch_count += 1
+
+        # Drop invalid images
+        batch = [data for data in batch if data is not None]
+
+        paths, imgs, bb_targets = list(zip(*batch))
+
+        # Selects new image size every tenth batch
+        if self.multiscale and self.batch_count % 10 == 0:
+            self.img_size = random.choice(
+                range(self.min_size, self.max_size + 1, 32)
+            )
+
+        # Resize images to input shape
+        imgs = torch.stack([resize(img, self.img_size) for img in imgs])
+
+        # Add sample index to targets
+        for i, boxes in enumerate(bb_targets):
+            boxes[:, 0] = i
+        bb_targets = torch.cat(bb_targets, 0)
+
+        return paths, imgs, bb_targets
+
+    def __len__(self):
+        return len(self.img_files)
+
+
+class my_dataset_LMDB_PKL(Dataset):
     def __init__(
         self, list_path, img_size=416, multiscale=True, transform=None
     ):
@@ -203,13 +325,115 @@ class my_dataset_LMDB(Dataset):
 
     def read_pkl(self, key):
         data_str = self.key_labels[key].decode("utf-8").split("\n")
-        result = np.array([list(map(float, s.split())) for s in data_str if s])
-        return result
+        label = np.array([list(map(float, s.split())) for s in data_str if s])
+        return label
 
     def __getitem__(self, index):
         img = self.read_lmdb(self.image_labels[index])
         boxes = self.read_pkl(self.image_labels[index])
-        print(img, boxes)
+        # print(img, boxes)
+        print(self.image_labels[index])
+        # -----------
+        #  Transform
+        # -----------
+        if self.transform:
+            try:
+                img, bb_targets = self.transform((img, boxes))
+            except Exception:
+                print("Could not apply transform.")
+                return
+        return None, img, bb_targets
+
+    def collate_fn(self, batch):
+        self.batch_count += 1
+
+        # Drop invalid images
+        batch = [data for data in batch if data is not None]
+
+        __, imgs, bb_targets = list(zip(*batch))
+
+        # Selects new image size every tenth batch
+        if self.multiscale and self.batch_count % 10 == 0:
+            self.img_size = random.choice(
+                range(self.min_size, self.max_size + 1, 32)
+            )
+
+        # Resize images to input shape
+        imgs = torch.stack([resize(img, self.img_size) for img in imgs])
+
+        # Add sample index to targets
+        for i, boxes in enumerate(bb_targets):
+            boxes[:, 0] = i
+        bb_targets = torch.cat(bb_targets, 0)
+
+        return __, imgs, bb_targets
+
+    def __len__(self):
+        return len(self.image_labels)
+
+
+class my_dataset_HDF5_PKL(Dataset):
+    def __init__(
+        self, list_path, img_size=416, multiscale=True, transform=None
+    ):
+        self.db_path = DB_PATH
+        self.pkl_path = PKL_PATH
+        self.key_labels = {}
+
+        with open(list_path, "r") as file:
+            self.img_files = file.readlines()
+        self.image_labels = [
+            path.split("/")[-1].split(".")[0] for path in self.img_files
+        ]
+
+        # Delay loading LMDB data until after initialization to avoid "can't pickle Environment Object error"
+        self._init_db()
+        self._init_pkl()
+
+        self.img_size = img_size
+        self.max_objects = 100
+        self.multiscale = multiscale
+        self.min_size = self.img_size - 3 * 32
+        self.max_size = self.img_size + 3 * 32
+        self.batch_count = 0
+        self.transform = transform
+
+    def _init_db(self):
+        self.hf = h5py.File(self.db_path, "r")  # open a hdf5 file
+
+    def _init_pkl(self):
+        with open(self.pkl_path, "rb") as handle:
+            self.key_labels = pickle.load(handle)
+
+    def read_hdf5(self, key):
+        img_bytes = np.array(self.hf[str(key)])  # read image in bytes
+        img = mmcv.imfrombytes(
+            img_bytes, flag="color", channel_order="rgb"
+        )  # convert to array
+        return img
+
+    def read_pkl(self, key):
+        data_str = self.key_labels[key].decode("utf-8").split("\n")
+        label = np.array(
+            [list(map(float, s.split())) for s in data_str if s]
+        ).reshape(-1, 5)
+        return label
+
+    def __getitem__(self, index):
+        try:
+            img = self.read_hdf5(self.image_labels[index])
+        except Exception:
+            print(f"Could not read image '{self.image_labels[index]}'.")
+            return
+
+        try:
+            boxes = self.read_pkl(self.image_labels[index])
+        except Exception:
+            print(f"Could not read label '{self.image_labels[index]}'.")
+            return
+
+        # print(img, boxes)
+        # print(self.image_labels[index])
         # -----------
         #  Transform
         # -----------
